4918d0058d4f6d765844657991e6be7b8dca0052
khactam <khactam95@gmail.com>
2019-09-03 19:45:23 +0300

    cloud changes

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,8 @@
-Parse.Cloud.define('hello', function(req, res) {
-  return 'cac';
+Parse.Cloud.define('getArea', function(req, res) {
+  let query = new Parse.Query('Area')
+  query.find({ useMasterKey: true }).then((response) =>{
+    return response
+  }, (error) =>{
+    console.log(error);
+  })
 });
