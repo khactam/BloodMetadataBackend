9aa4d2b3e52689f2e2dac9252417643786052e55
khactam <khactam95@gmail.com>
2019-09-05 17:17:21 +0300

    cloud code function

--- cloud/main.js
+++ cloud/main.js
@@ -2,19 +2,19 @@ Parse.Cloud.define('getArea', function (req, res) {
   const query = new Parse.Query('Area')
   return query.find({ useMasterKey: true })
 });
-Parse.Cloud.define('getSpec', function (request) {
-  let areaName = request.area
-  let areaObj = getArea(areaName)
-  areaObj.low = areaObj.get('frequencies')[0]
-  areaObj.high = areaObj.get('frequencies')[1]
-  return { "low": areaObj.low, "high": areaObj.high}
-})
-function getArea(areaName) {
-  const query = new Parse.Query('Area')
+async function getArea (areaName) {
+  let query = new Parse.Query('Area')
   query.equalTo('name', areaName)
-  return query.first()
+  return query.first({ useMasterKey: true })
 }
-function getSpecs() {
+function getSpecs () {
   const query = new Parse.Query('Specs')
   return query.first().get('spectrum')
 }
+Parse.Cloud.define('getSpec', async function (request) {
+  let areaName = request.params.area
+  let areaObj = getArea(areaName)
+  // areaObj.low = areaObj.get('frequencies')[0]
+  // areaObj.high = areaObj.get('frequencies')[1]
+  return areaObj
+})
