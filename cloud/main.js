07d98314271857f2f05e5a2468be20acc467ccbb
khactam <khactam95@gmail.com>
2019-09-05 16:33:24 +0300

    few changes

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,4 @@
-Parse.Cloud.define("averageStars", async (request) => {
-  return 'vailon'
+Parse.Cloud.define('getArea', function (req, res) {
+  const query = new Parse.Query('Area')
+  return query.find({ useMasterKey: true })
 });
